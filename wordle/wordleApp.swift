0853e41013786062ff825c3af8cd674259b9377f
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-03-23 15:48:37 +0800

    Initial Commit

---
+++ wordle/wordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordleApp.swift
+//  wordle
+//
+//  Created by tingyang on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct wordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
